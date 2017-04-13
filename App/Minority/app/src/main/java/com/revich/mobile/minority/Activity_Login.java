343341e8243110b2e76b2b7f703845d512fce658
igalrevich <igalrevich@gmail.com>
2017-04-13 20:03:37 -0300

    PantallasMinority

---
+++ App/Minority/app/src/main/java/com/revich/mobile/minority/Activity_Login.java
@@ -0,0 +1,13 @@
+package com.revich.mobile.minority;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_login);
+    }
+}
